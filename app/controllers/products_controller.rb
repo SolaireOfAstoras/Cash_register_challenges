f84ec3c5692233998b762b222608526869ad4ff4
SolaireOfAstoras <pierrealexandre.duquenne@gmail.com>
2021-06-22 11:27:01 +0200

    model+viws+controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    @All_products = Product.all
   end
 
   def check_out
+    if params[:cart].nil? || params[:cart] == [""]
+      redirect_to "/"
+    else
+      @my_all_elemts = Product.my_cart(params)
+    end
   end
 end
